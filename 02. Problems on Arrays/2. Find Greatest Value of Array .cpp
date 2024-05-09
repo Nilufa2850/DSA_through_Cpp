bba387c1c4612178c39cf90dfe2bdbd548d1b80f
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2024-05-09 12:14:05 +0530

    Create 2. Find Greatest Value of Array .cpp

---
+++ 02. Problems on Arrays/2. Find Greatest Value of Array .cpp
@@ -0,0 +1,41 @@
+/*2.Define a function to find the greatest element in the array*/
+
+#include<iostream>
+using namespace std ;
+
+int greatest (int a[] , int size)
+{
+    int i , j , temp ;
+
+    for (i=0 ; i<=size-2 ; i++)
+        for (j=i+1 ; j<=size-1 ; j++)
+            if (a[i] > a[j])
+                temp = a[i] ;
+
+    return temp ;
+}
+int main ()
+{
+    int *ptr  ;
+    
+    int size , i ;
+
+    cout<<"Enter Array Sizes : " ;
+    cin>>size ;
+
+    if (ptr != NULL)
+        delete []ptr ;
+    ptr = new int [size] ;
+
+    cout<<"Enter "<<size<<" Elements\n" ;
+    for (i=0 ; i<size ; i++)
+        cin>>ptr[i] ;
+
+    for (i=0 ; i<size ; i++)
+        cout<<ptr[i]<<"  ";
+
+    cout<<"\nGreatest Element is "<<greatest(ptr,size) ;
+
+    delete []ptr ;
+    return 0 ;
+}
